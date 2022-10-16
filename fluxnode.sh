fe8791f1c795baa2e5889afe960f41c413458bab
Nhut Do <mr.nhut@gmail.com>
2022-10-16 16:47:48 +0300

    Update text.

--- fluxnode.sh
+++ fluxnode.sh
@@ -1,3 +1,3 @@
 sudo make install
 sudo systemctl enable --now wondershaper.service
-echo Done!
+echo Network bandwidth limit set for Cumulus. You can change it to other node running following command: sudo nano /etc/systemd/wondershaper.conf
