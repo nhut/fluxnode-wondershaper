6e0616b7f6378b67e34ec9ecca36e9d2a7c9e318
Nhut Do <mr.nhut@gmail.com>
2022-10-23 22:16:51 +0300

    Add important notice.

--- create-autostart-service.sh
+++ create-autostart-service.sh
@@ -1,3 +1,5 @@
 sudo make install
 sudo systemctl enable --now wondershaper.service
 echo Network bandwidth limit set for Cumulus. You can change it to other node running following command: sudo nano /etc/systemd/wondershaper.conf
+ip -br l | awk '$1 !~ "lo|vir|wl" { print $1}' | grep ens
+echo IMPORTANT! Check that interface defined in the /etc/systemd/wondershaper.conf config file is correct, same what is printed above.
